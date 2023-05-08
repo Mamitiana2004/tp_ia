cec69b867f84c534fd166d82133bd839e6c5f5ca
Diary11 <99733125+Diary11@users.noreply.github.com>
2023-05-09 01:42:03 +0300

    asset

--- TP S6 P14 Web design Mai 2022/resources/views/adminLogin.blade.php
+++ TP S6 P14 Web design Mai 2022/resources/views/adminLogin.blade.php
@@ -8,13 +8,13 @@
  
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<link href="{{asset('admin/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all">
+<link href="admin/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
 <!-- Custom Theme files -->
-<link href="{{asset('admin/css/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
+<link href="admin/css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <!--js-->
-<script src="{{asset('admin/js/jquery-2.1.1.min.js')}}"></script> 
+<script src="admin/js/jquery-2.1.1.min.js"></script> 
 <!--icons-css-->
-<link href="{{asset('admin/css/font-awesome.css')}}" rel="stylesheet"> 
+<link href="admin/css/font-awesome.css" rel="stylesheet"> 
 <!--Google Fonts-->
 <!--link href='//fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'-->
 <!--link href='//fonts.googleapis.com/css?family=Work+Sans:400,500,600' rel='stylesheet' type='text/css'-->
